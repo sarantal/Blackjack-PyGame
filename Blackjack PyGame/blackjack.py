be4b024af45c35e5a5f09dab3d8b71e54d316ca2
sarantal <44088639+sarantal@users.noreply.github.com>
2021-08-06 16:34:07 +0300

    Add files via upload

--- Blackjack PyGame/blackjack.py
+++ Blackjack PyGame/blackjack.py
@@ -32,15 +32,7 @@ font_button = pygame.font.Font("COPRGTL.TTF", 28)
 background = "background.jpg"
 random.seed()
 card_sprites = pygame.image.load(os.path.join("deck.png")).convert_alpha()
-dealers_cards = []
-players_cards = []
 buttons = []
-
-deck = ["s1","s2","s3","s4","s5","s6","s7","s8","s9","s10","s11","s12","s13",
-        "h1","h2","h3","h4","h5","h6","h7","h8","h9","h10","h11","h12","h13",
-        "d1","d2","d3","d4","d5","d6","d7","d8","d9","d10","d11","d12","d13",
-        "c1","c2","c3","c4","c5","c6","c7","c8","c9","c10","c11","c12","c13"]
-
 
 
 """ CLASSES """
@@ -53,24 +45,20 @@ class Card:
         self.value = value
         self.suite = suite
         self.position = position        # just x value, y fixed by dealer/player
-        (self.x, self.y) = position     # as cards are added to table, move them closer: card.x -= 20
+        (self.x, self.y) = position
         self.draw()
 
     def draw(self):
         # choose sprite
         if self.facing == "down":
             self.card_sprite = card_sprites.subsurface(4*98, 4*144, 98, 144)
-
         else:
             self.card_sprite = card_sprites.subsurface((self.value-1)*98, (self.suite-1)*144, 98, 144)
-
         # position
         self.rect = self.card_sprite.get_rect()
         self.rect.topleft = self.position
-
         # blit sprite
         window.blit(self.card_sprite, self.rect)
-
 
 
 class Button:
@@ -117,8 +105,7 @@ class Button:
         #self.shadow_rect = self.text.get_rect()
         self.shadow_rect = pygame.Rect(self.x, self.y, 150, 40)
         self.shadow_rect.topleft = self.pos
-        self.shadow_rect.move_ip(2, 3)    
-
+        self.shadow_rect.move_ip(2, 3)
 
 
 """ FUNCTIONS """
@@ -137,7 +124,7 @@ def draw_text(message, font, color, position):
 
 def deal_card(target, facing):
 
-    global deck, dealers_cards, players_cards
+    global deck, dealers_cards, players_cards, face_down_card
 
     card_str = random.choice(deck)
     if card_str[0] == "s":
@@ -151,21 +138,63 @@ def deal_card(target, facing):
     cardvalue = int(card_str[1:])
     deck.remove(card_str)
 
-    print(card_str)
+    print(card_str)         # for debugging
 
     # Dealers face down card
     if facing == "down":
         face_down_card = card_str
         dealers_cards.append(Card("dealer", "down", cardvalue, suite, (250, 150)))
-        
-        # TODO: adjust coords afterwards by: object.x = object.x - 20
-        #       so coords dont have to be hardcoded
 
     elif target == "player":
-        players_cards.append(Card("player", "up", cardvalue, suite, (250, 400)))
+        x_coord = 250 + len(players_cards)*100
+        players_cards.append(Card("player", "up", cardvalue, suite, (x_coord, 400)))
+        check_score()
+
+    # TODO: elif target == "dealer":
 
 
-#def shuffle():                                                 TODO
+def check_score():
+
+    if len(dealers_cards) > 1:              # update dealers score
+        cardvalues = []
+        # TODO
+
+    else:                                   # update players score
+        cardvalues = []
+        for card in players_cards:
+            if card.value > 10:             # Jacks, Queens and Kings worth 10
+                cardvalues.append(10)
+            elif card.value == 1:           # Ace worth 11 unless sum is above 21
+                cardvalues.append(11)
+            else:
+                cardvalues.append(card.value)
+        players_score = sum(cardvalues)
+        if players_score > 21:
+            if 11 in cardvalues:
+                cardvalues.remove(11)
+                cardvalues.append(1)
+                players_score = sum(cardvalues)
+        if players_score > 21:
+            print("Player is bust! (" + str(players_score) + ")")
+
+        # DEBUGGING
+        print("Players score: " + str(players_score))
+        print(cardvalues)
+
+    # TODO: check if we have a winner
+
+
+def shuffle():
+
+    global deck, dealers_cards, players_cards
+
+    deck = ["s1","s2","s3","s4","s5","s6","s7","s8","s9","s10","s11","s12","s13",
+            "h1","h2","h3","h4","h5","h6","h7","h8","h9","h10","h11","h12","h13",
+            "d1","d2","d3","d4","d5","d6","d7","d8","d9","d10","d11","d12","d13",
+            "c1","c2","c3","c4","c5","c6","c7","c8","c9","c10","c11","c12","c13"]
+
+    dealers_cards = []
+    players_cards = []
 
 
 def handle_mouse_move():                                        # for button hiliting
@@ -175,7 +204,31 @@ def handle_mouse_move():                                        # for button hil
             button.hilite = True
         else:
             button.hilite = False
-        
+
+
+def handle_mouse_click():
+
+    for button in buttons:
+        if button.rect.collidepoint(pygame.mouse.get_pos()):
+            if button.name == "Quit":
+                pygame.quit()
+                quit()
+            elif button.name == "Stay":
+                dealers_turn()
+            elif button.name == "Card":
+                deal_card("player", "up")
+
+
+def dealers_turn():                             # TODO
+
+    global dealers_cards
+
+    dealers_cards[0].facing = "up"
+    # DELAY
+    # deal_card("dealer", "up")
+    # DELAY AND DECIDE: STAY/CARD
+    # REPEAT UNTIL STAY/BUST
+
 
 
 """ MAIN """
@@ -184,25 +237,26 @@ def main():
 
     global buttons
 
+    # SETUP
     bg_image = pygame.image.load(os.path.join(background)).convert()
+    shuffle()
+    button_card = Button("Card", (850, 200))
+    button_stay = Button("Stay", (850, 250))
+    button_quit = Button("Quit", (850, 300))
+    buttons.append(button_card)
+    buttons.append(button_stay)
+    buttons.append(button_quit)
 
-
-    # TESTING
     deal_card("dealer", "down")
     deal_card("player", "up")
-    button1 = Button("Card", (850, 200))
-    button2 = Button("Stay", (850, 250))
-
-    buttons.append(button1)
-    buttons.append(button2)
 
 
     while True:
 
         fpsClock.tick(FPS)
-        window.blit(bg_image, (0, 0))   
+        window.blit(bg_image, (0, 0))
 
-        # TESTING
+        # DRAW OBJECTS
         draw_text("BLACKJACK", font_big, WHITE, (400, 50))
         for button in buttons:
             button.draw()
@@ -220,6 +274,9 @@ def main():
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 pygame.quit()
                 quit()
+
+            elif event.type == MOUSEBUTTONDOWN:
+                handle_mouse_click()
 
             elif event.type == pygame.MOUSEMOTION:
                 handle_mouse_move()
